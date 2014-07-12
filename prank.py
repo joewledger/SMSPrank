52a1e88336e7d428e7454312d22b42135981190d
joewledger <joewledger@gmail.com>
2014-07-11 20:57:39 -0400

    Task scheduling fixed

--- prank.py
+++ prank.py
@@ -4,8 +4,7 @@ import os
 import argparse
 import platform
 import sys
-#Crontab for Unix based systems
-import crontab
+from crontab import CronTab
 
 parser = argparse.ArgumentParser(description='Credentials and facts reciever information')
 parser.add_argument("username")
@@ -13,22 +12,31 @@ parser.add_argument("password")
 parser.add_argument("from_address")
 parser.add_argument("to_address")
 parser.add_argument("reciever_name")
+
 args = parser.parse_args()
 
 op_system = platform.system()
+
 #Windows Task Scheduler
 if(op_system == "Windows"):
 	#check if task has been scheduled, if not schedule it
 	if(os.system("schtasks /QUERY /TN Prank") != 0):
 		py_exe = "C:\Python" + sys.version[0:3].replace(".","") + "\python.exe"
 		py_file = os.path.realpath(__file__)
-		os.system('schtasks /Create /SC HOURLY /TN Prank /TR "%s %s" %s %s %s %s %s' 
-			% (py_exe,py_file, args.username,args.password,args.from_address,arg.to_address,args.reciever_name))
+		task = 'schtasks /Create /SC minute /mo 20 /TN Prank /TR "%s %s %s %s %s %s %s"' % (py_exe,py_file, args.username,args.password,args.from_address,args.to_address,args.reciever_name)
+		os.system(task)
 else:
 	#Crontab for Unix based systems 
 	if(op_system == "Linux" or op_system == "Darwin"):
 		print('UNIX')
 		#check if task has been scheduled, if not schedule it
+		tab = CronTab(user=True)
+		py_exe = "C:\Python" + sys.version[0:3].replace(".","") + "\python.exe"
+		py_file = os.path.realpath(__file__)
+		task = "%s %s %s %s %s %s %s" % (py_exe,py_file, args.username,args.password,args.from_address,args.to_address,args.reciever_name)
+		cron_job = tab.new(task)
+		cron_job.setall('*/20 * * * * ')
+		tab.write()
 	else: 
 		print("Only Windows, Mac, and Linux are supported")
 
